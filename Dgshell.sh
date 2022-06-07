cb13e6e45ffa039ba59e4e7a5a7ba31776df5ee4
Toyoui <91711069+Toyoui@users.noreply.github.com>
2022-06-07 10:15:15 +0800

    Create Dgshell.sh

---
+++ Dgshell.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
+
+arch=$(arch)
+
+if [[ $arch == "x86_64" || $arch == "x64" || $arch == "amd64" ]]; then
+    arch="64"
+elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
+    arch="arm64-v8a"
+elif [[ $arch == "s390x" ]]; then
+    arch="s390x"
+else
+    arch="64"
+    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
+fi
+
+echo "架构: ${arch}"
+
+if [ "$(getconf WORD_BIT)" != '32' ] && [ "$(getconf LONG_BIT)" != '64' ] ; then
+    echo "本脚本不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
+    exit 2
+fi
+
+install_docker() {
+#安装sudo
+apt -y install sudo
+#更新 apt 包索引
+sudo apt-get update
+#安装 apt 依赖包，用于通过 HTTPS 来获取仓库
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg2 \
+    software-properties-common
+#添加 Docker 的官方 GPG 密钥
+curl -fsSL https://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo apt-key add
+#使用以下指令设置稳定版仓库
+sudo add-apt-repository \
+   "deb [arch=${arch}] https://mirrors.ustc.edu.cn/docker-ce/linux/debian \
+  $(lsb_release -cs) \
+  stable"
+#安装 Docker Engine-Community
+sudo apt-get update
+#安装最新版本的 Docker Engine-Community 和 containerd
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+}
+
+install_tools() {
+apt install wget curl unzip tar cron socat -y
+}
+
+echo -e "${green}开始安装${plain}"
+install_docker
+install_tools $1
+
+
+
+
